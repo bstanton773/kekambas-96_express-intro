3ce1beb2d9f44e8b8e59c158aec11c51bd1b3262
Brian <bstanton773@gmail.com>
2022-09-06 15:01:12 -0500

    Added some middleware examples

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = 3000;
 
 app.set('view engine', 'ejs')
 
+app.use(logger)
+
 app.get('/', (req, res) => {
+    console.log(req.test);
     res.render('index', {firstName: 'Brian', lastName: 'Stanton'});
 })
 
@@ -30,20 +33,43 @@ app.get('/users', (req, res) => {
     res.render('users', { users })
 })
 
+app.param('id', getUser);
+
 app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    for (let user of users){
-        if (user.id == id){
-            res.render('user', { user })
-        }
-    }
-    res.send({error: `User with id ${id} does not exist`})
+    res.render('user', { user: req.user })
+})
+
+app.put('/users/:id', (req, res) => {
+    res.send('Update User')
+})
+
+app.delete('/users/:id', (req, res) => {
+    res.send('Delete User')
 })
 
 app.get('/test/:testId/:testName/:abc/:xyz', (req, res) => {
     console.log(req.params);
     res.send('Test')
 })
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    req.test = 123
+    next();
+}
+
+function getUser(req, res, next, id){
+    for (let u of users){
+        if (u.id == id){
+            req.user = u
+        }
+    }
+    if (req.user){
+        next()
+    } else {
+        res.send({error: `No user with an id of ${id}`})
+    }
+}
 
 app.listen(port, () => {
     console.log(`Hello World app listening on port ${port}`)
