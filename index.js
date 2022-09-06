b3e2a4976dfdd5259491ba30cd7c6a2e0f1bb044
Brian <bstanton773@gmail.com>
2022-09-06 11:55:32 -0500

    Add ejs template engine and set up express app to use ejs

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!!!');
+    res.render('index');
+})
+
+app.get('/abc', (req, res) => {
+    res.send('ABC!')
 })
 
 app.listen(port, () => {
